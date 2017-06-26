82fdcf32331f18b5ecb98140503c99b31ff5bcd6
Dominic <awesome3165@gmail.com>
2017-06-25 22:42:19 -0400

    Added some more code wooo

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,44 @@ use std::io;
 use std::io::Write;
 
 // Let's begin our definitions of the objects we'll use to control the LORA radios
+struct Controller {
+	// Control pin definitions
+	m0: Pin,
+	m1: Pin,
+	aux: Pin
+}
+
+// Controller functions
+impl Controller {
+
+	// Controller should be instantiated with the 3 main pins to control the radio. M0, M1, and AUX.
+	pub fn new(m0_pin_num: u64, m1_pin_num: u64, aux_pin_num: u64) -> Controller {
+		let m0_pin = Pin::new(m0_pin_num);
+		let m1_pin = Pin::new(m1_pin_num);
+		let aux_pin = Pin::new(aux_pin_num);
+
+		//TODO: Find a better way to do error handling
+		match m0_pin.set_direction(Direction::Out) {
+			Ok(()) => println!("good"),
+			Err(e) => println!("{}",e)
+		}
+		match m1_pin.set_direction(Direction::Out) {
+			Ok(()) => println!("good"),
+			Err(e) => println!("{}",e)
+		}
+		match aux_pin.set_direction(Direction::Out) {
+			Ok(()) => println!("good"),
+			Err(e) => println!("{}",e)
+		}
+
+
+		Controller{m0: Pin::new(m0_pin_num), m1: Pin::new(m1_pin_num), aux: Pin::new(aux_pin_num)}
+	}
+
+	pub fn get_control_gpio_pins(&self) -> (u64, u64, u64) {
+		(self.m0.get_pin_num(), self.m1.get_pin_num(), self.aux.get_pin_num())
+	}
+}
 
 
 
@@ -34,11 +72,17 @@ fn interrupt(pin: u64) -> sysfs_gpio::Result<()>{
 
 fn main() {
 
+	/*
 	println!("Monitoring pin 0");
 	match interrupt(0) {
 		Ok(()) => println!("Interrupting Complete!"),
 		Err(err) => println!("Error: {}", err),
-}
+	}
 	println!("Monitoring finished!");
+	*/
+
+	let e23_controller = Controller::new(1,2,3);
+	let (m0_pin, m1_pin, aux_pin) = e23_controller.get_control_gpio_pins();
+	println!("M0: {} M1: {} AUX: {}", m0_pin, m1_pin, aux_pin);
 
 }
