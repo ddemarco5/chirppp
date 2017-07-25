44ad17be01bd051b05b5c76d5872644b1fe8dd07
Dominic <awesome3165@gmail.com>
2017-07-24 22:57:13 -0400

    created initial state machine for the serial emulator. need to work out some bugs.

--- build_multi.sh
+++ build_multi.sh
@@ -11,8 +11,8 @@ cargo build --target armv7-unknown-linux-gnueabihf
 
 # scp our binary over to the vocore
 echo "Vocore"
-scp target/mipsel-unknown-linux-musl/debug/chirppp root@192.168.1.26:/mnt/sda1
+scp target/mipsel-unknown-linux-musl/debug/chirppp root@192.168.1.11:/mnt/sda1
 
 # scp out binary over to the C.H.I.P.
 echo "C.H.I.P."
-scp target/armv7-unknown-linux-gnueabihf/debug/chirppp chip@192.168.1.28:/home/chip
+scp target/armv7-unknown-linux-gnueabihf/debug/chirppp root@192.168.1.14:/root
--- src/main.rs
+++ src/main.rs
@@ -92,6 +92,7 @@ fn main() {
 	// Get args that were passed
 	let args: Vec<String> = env::args().collect();
 	let platform = &args[1];
+	let send_or_receive = &args[2];
 
 	let mut e32_driver;
 	let mut testconfig = RadioConfig::new();
@@ -126,32 +127,135 @@ fn main() {
 	
 
 	testconfig.set_transmit_power("20dBm");
-	testconfig.set_air_rate("10k");
+	testconfig.set_air_rate("1k");
 	testconfig.set_address("FFFF"); // Channel 0
 	e32_driver.set_mode(RadioMode::General);
 	e32_driver.write_config(testconfig);
 	
 	
-	
-
-	//println!("rc: {:?}, amaster: {:?}, aslave: {:?}, name: {:?}",test_openpty,amaster,aslave,name);
 
 	let mut pty_master_file = create_pty_pair().unwrap();
 	//let mut bufreader = BufReader::new(pty_master_file);
 
-	// Just read from the port and send packet along until we send a kill signal
+	/* I don't want to do obect oriented stuff for this loop... might do it later, for now, states
+	0: no operation, shouldn't ever be this.
+	1: receive packet
+	2: flush received to output
+	3: read from serial and send packet
+	4: send previous packet again
+	5: send heartbeat packet
+	6: send retry packet
+	*/
+	let mut state = 0;
+	match &send_or_receive[..] {
+		"s" => state = 3,
+		"r" => state = 1,
+		_ => panic!("Invalid option specified, must be 'r' or 's'")
+	}
+	let CMD_BYTE_HEARTBEAT = 2;
+	let CMD_BYTE_RETRY = 1;
+	let mut previous_packet: Vec<u8> = vec![0];
+	let mut received_packet: Vec<u8> = vec![0];
+	// Start our operation loop
 	loop {
-		let mut read_vec: Vec<u8> = vec![0;256];
-		let num_bytes_read = pty_master_file.read(&mut read_vec).unwrap();
-		read_vec.truncate(num_bytes_read);
-		print!("Sending: {}", String::from_utf8(read_vec.clone()).unwrap());
-		e32_driver.send_packet(&read_vec,5000).unwrap();
-		let recv_vec = e32_driver.receive_packet(120_000).unwrap();
-		println!("Received: {}", String::from_utf8(recv_vec.clone()).unwrap());
-		pty_master_file.write_all(&recv_vec).unwrap();
+
+		// for debug slow our whole loop down to 1hz
+		sleep(Duration::from_secs(1));
+
+		match state {
+			// receive packet
+			1 => {
+				println!("Receive packet (1)");
+				let mut packet_read = vec![0];
+				
+				match e32_driver.receive_packet(10_000) {
+					Ok(bytes_read) => { packet_read = bytes_read; },
+
+					Err(e) => {
+						// Timed out, we need to send a retry packet
+						state = 6;
+						// hop out of this match and continue the loop
+						continue;
+					}
+				}
 
 
-		//sleep(Duration::from_secs(1));
+				if packet_read.len() == 0 { panic!("Read 0 bytes, something is wrong."); }
+
+				// Check our command byte and take appropriate action
+				match packet_read[0] {
+					1 => {
+						// this is a retry message, send previous packet again
+						state = 4;
+					}
+					2 | 0 => {
+						// they had no data to send, or this is regular data
+						state = 2;
+					}
+					_ => {
+						// garbage in command byte, invalid state, panic
+						panic!("Garbage in command byte, corrupt packet? {:?}", packet_read[0]);
+					}
+				}
+			}
+			// flush received data to output
+			2 => {
+				println!("flush received data to output (2)");
+				if received_packet.len() == 0 { panic!("Attempted to output blank packet, this should never happen"); }
+				// send our output without the command byte
+				pty_master_file.write_all(&received_packet[1..]).expect("Failed to output data to pty");
+				state = 3;
+			}
+			// read from serial and send packet
+			3 => {
+				println!("read from serial and send packet (3)");
+				let mut packet_to_send: Vec<u8> = vec![0;57];
+				previous_packet = packet_to_send.clone();
+				// reads up to 57 bytes from serial
+				let num_bytes_read = pty_master_file.read(&mut packet_to_send).unwrap();
+				if num_bytes_read == 0 {
+					// got no data from serial. send heartbeat instead.
+					// TODO: add some delay here. There is no point in writing heartbeats as fast as we can
+					state = 5;
+					continue;
+				}
+				// make sure our command 
+				let mut send_packet_w_cmd = vec![0];
+				// push our data after the command byte
+				send_packet_w_cmd.append(&mut packet_to_send);
+				e32_driver.send_packet(&send_packet_w_cmd, 5000).expect("Error sending packet");
+				// our turn to read a packet
+				state = 1;
+			}
+			// send previous packet again
+			4 => {
+				println!("send previous packet again (4)");
+				let mut send_packet_w_cmd = vec![0];
+				// push our data after the command byte
+				send_packet_w_cmd.append(&mut previous_packet.clone());
+				e32_driver.send_packet(&send_packet_w_cmd, 5000).expect("Error sending previous packet");
+				// our turn to read a packet
+				state = 1;
+			}
+			// send heartbeat packet
+			5 => {
+				println!("send heartbeat packet (5)");
+				let mut send_packet_w_cmd = vec![CMD_BYTE_HEARTBEAT];
+				e32_driver.send_packet(&send_packet_w_cmd, 5000).expect("Error sending heartbeat packet");
+				// our turn to read a packet
+				state = 1;
+			}
+			// send retry packet
+			6 => {
+				println!("send retry packet (6)");
+				let mut send_packet_w_cmd = vec![CMD_BYTE_RETRY];
+				e32_driver.send_packet(&send_packet_w_cmd, 5000).expect("Error sending heartbeat packet");
+				// our turn to read a packet
+				state = 1;
+			}
+			_ => panic!("Invalid state... some code is bad")
+		}; // we don't want to return from the match to keep the loop going
+
 	}
 
 }
