09f41adfc624a51835027b9aeb4e510e213aca9d
Dominic <awesome3165@gmail.com>
2017-07-27 23:32:40 -0400

    Simple version of protocol is mostly working

--- build_multi.sh
+++ build_multi.sh
@@ -11,8 +11,8 @@ cargo build --target armv7-unknown-linux-gnueabihf
 
 # scp our binary over to the vocore
 echo "Vocore"
-scp target/mipsel-unknown-linux-musl/debug/chirppp root@192.168.1.11:/mnt/sda1
+scp target/mipsel-unknown-linux-musl/debug/chirppp root@192.168.1.14:/mnt/sda1
 
 # scp out binary over to the C.H.I.P.
 echo "C.H.I.P."
-scp target/armv7-unknown-linux-gnueabihf/debug/chirppp root@192.168.1.14:/root
+scp target/armv7-unknown-linux-gnueabihf/debug/chirppp root@192.168.1.16:/root
--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,20 @@ use std::thread::sleep;
 
 use std::time::Duration;
 use std::fs::File;
-use std::os::unix::io::IntoRawFd;
+use std::os::unix::io::AsRawFd;
 use std::os::unix::io::FromRawFd;
 use std::io::Read;
-use std::io::BufReader;
+
 use std::io::Write;
 use std::io::Error;
 use std::io::ErrorKind;
 //use std::io::prelude::*;
 
-extern crate floating_duration;
-//use floating_duration::TimeAsFloat;
-
+// For driver init
 extern crate serial;
+
+//extern crate floating_duration;
+//use floating_duration::TimeAsFloat;
 
 extern crate lora_driver;
 use lora_driver::RadioConfig;
@@ -30,10 +31,65 @@ extern crate libc;
 use libc::termios;
 //use libc::winsize;
 
+extern crate nix;
+use nix::sys::select::select;
+use nix::sys::select::FdSet;
+use nix::sys::time::TimeVal;
 
 // Testing for Bryant
 //extern crate serialport;
 
+
+static CMD_BYTE_HEARTBEAT: u8 = 1;
+static CMD_BYTE_RETRY: u8 = 2;
+
+static PACKET_TIMEOUT: u32 = 10_000;
+static PTY_TIMEOUT: u32 = 5_000;
+
+static XON: u8 = 17; // software flow control. dec 19 is XON
+static XOFF: u8 = 19; // and 19 is XOFF
+
+
+pub fn read_timeout(file: &mut File, mut buffer: &mut Vec<u8>, duration: u32) -> Result<(), Error> {
+	let file_fd = file.as_raw_fd();
+	//let mut blank_fd_set = FdSet::new();
+	let mut file_fd_set = FdSet::new();
+	file_fd_set.insert(file_fd);
+	let duration_i64 = duration as i64;
+	let mut timeval_millis = TimeVal::milliseconds(duration_i64);
+	match select(file_fd+1, Some(&mut file_fd_set), None, None, Some(&mut timeval_millis)) {
+		Ok(1) => {
+			// Ok to read from file
+
+			// debug
+			//println!("Select succeeded with a 1");
+			// Send XON flow control, we're ready to receive serial data
+			//file.write_all(&[XON;1]).unwrap();
+			// Read tup to the max number of bytes to fill the packet (probably 58)
+			file.read(&mut buffer).unwrap();
+			// We've read as much as we can fit into a packet, send transmit off
+			//file.write_all(&[XOFF;1]).unwrap();
+
+			Ok(())
+		}
+		Ok(0) => {
+			// Timeout
+			//println!("Select timed out");
+			Err(Error::new(ErrorKind::Other, "Read pty timed out"))
+		}
+		Ok(_) => {
+			// Something else we don't expect
+			panic!("Got a weird return code from select");
+		}
+		Err(e) => {
+			// Timeout
+			panic!("Select had an error {}",e);
+		}
+	}
+}
+
+
+// TODO: look into using the rust nix:: crate to do this
 pub fn create_pty_pair() -> Result<File, Error> {
 	// I do NOT like the config targets... but for now it may be necessary
 	#[cfg(any(target_arch = "arm", target_arch = "x86_64"))]
@@ -152,80 +208,99 @@ fn main() {
 		"r" => state = 1,
 		_ => panic!("Invalid option specified, must be 'r' or 's'")
 	}
-	let CMD_BYTE_HEARTBEAT = 2;
-	let CMD_BYTE_RETRY = 1;
-	let mut previous_packet: Vec<u8> = vec![0];
-	let mut received_packet: Vec<u8> = vec![0];
+
+	let mut previous_packet: Vec<u8> = vec![0;57];
+	let mut received_packet: Vec<u8> = vec![0;57];
 	// Start our operation loop
 	loop {
 
 		// for debug slow our whole loop down to 1hz
-		sleep(Duration::from_secs(1));
+		//sleep(Duration::from_secs(1));
 
 		match state {
 			// receive packet
 			1 => {
 				println!("Receive packet (1)");
-				let mut packet_read = vec![0];
+				let packet_read;
 				
-				match e32_driver.receive_packet(10_000) {
-					Ok(bytes_read) => { packet_read = bytes_read; },
+				match e32_driver.receive_packet(PACKET_TIMEOUT) {
+					Ok(bytes_read) => {
 
+						// TODO: this is a temp solution for falsely detecting a packet on the Radio
+						if bytes_read.len() == 0 { 
+							println!("False positive from radio, trying again.");
+							state = 1;
+							continue;
+						}
+
+
+						// check to make sure we haven't received a command packet
+						if bytes_read[0] == CMD_BYTE_RETRY {
+							// Send previous packet again
+							println!("Received a retry packet");
+							state = 4;
+							continue;
+						}
+						else if bytes_read[0] == CMD_BYTE_HEARTBEAT {
+							// Received heartbeat, write no data
+							println!("Received a heartbeat packet");
+							state = 3;
+							continue;
+						}
+						else if bytes_read[0] == 0 {
+							// Not a command byte, continue with the state routine
+							packet_read = bytes_read;
+						}
+						else {
+							// Some weird malformed shit, we should never see this
+							panic!("Corrupt command byte: {:?}", bytes_read);
+						}
+					},
 					Err(e) => {
 						// Timed out, we need to send a retry packet
 						state = 6;
 						// hop out of this match and continue the loop
 						continue;
 					}
-				}
+				}; // don't return, we're not done yet 
 
-
-				if packet_read.len() == 0 { panic!("Read 0 bytes, something is wrong."); }
-
-				// Check our command byte and take appropriate action
-				match packet_read[0] {
-					1 => {
-						// this is a retry message, send previous packet again
-						state = 4;
-					}
-					2 | 0 => {
-						// they had no data to send, or this is regular data
-						state = 2;
-					}
-					_ => {
-						// garbage in command byte, invalid state, panic
-						panic!("Garbage in command byte, corrupt packet? {:?}", packet_read[0]);
-					}
-				}
+				// write out packet to receive_packet, but not the command byte.
+				received_packet = packet_read[1..].to_vec();
+				state = 2;
+				
 			}
 			// flush received data to output
 			2 => {
 				println!("flush received data to output (2)");
 				if received_packet.len() == 0 { panic!("Attempted to output blank packet, this should never happen"); }
 				// send our output without the command byte
-				pty_master_file.write_all(&received_packet[1..]).expect("Failed to output data to pty");
+				pty_master_file.write_all(&received_packet).expect("Failed to output data to pty");
 				state = 3;
 			}
 			// read from serial and send packet
 			3 => {
 				println!("read from serial and send packet (3)");
 				let mut packet_to_send: Vec<u8> = vec![0;57];
-				previous_packet = packet_to_send.clone();
 				// reads up to 57 bytes from serial
-				let num_bytes_read = pty_master_file.read(&mut packet_to_send).unwrap();
-				if num_bytes_read == 0 {
-					// got no data from serial. send heartbeat instead.
-					// TODO: add some delay here. There is no point in writing heartbeats as fast as we can
-					state = 5;
-					continue;
+				match read_timeout(&mut pty_master_file, &mut packet_to_send, PTY_TIMEOUT) {
+					Ok(()) => {
+						// We read some bytes
+						// set the previous packet here
+						previous_packet = packet_to_send.clone();
+						// make sure our command byte is in front
+						println!("Sending data packet");
+						let mut send_packet_w_cmd = vec![0];
+						// push our data after the command byte
+						send_packet_w_cmd.append(&mut packet_to_send);
+						e32_driver.send_packet(&send_packet_w_cmd, 5000).expect("Error sending packet");
+						// our turn to read a packet
+						state = 1;
+					}
+					Err(e) => {
+						// Got no data from serial. Send heartbeat instead
+						state = 5;
+					}
 				}
-				// make sure our command 
-				let mut send_packet_w_cmd = vec![0];
-				// push our data after the command byte
-				send_packet_w_cmd.append(&mut packet_to_send);
-				e32_driver.send_packet(&send_packet_w_cmd, 5000).expect("Error sending packet");
-				// our turn to read a packet
-				state = 1;
 			}
 			// send previous packet again
 			4 => {
@@ -240,7 +315,7 @@ fn main() {
 			// send heartbeat packet
 			5 => {
 				println!("send heartbeat packet (5)");
-				let mut send_packet_w_cmd = vec![CMD_BYTE_HEARTBEAT];
+				let send_packet_w_cmd = vec![CMD_BYTE_HEARTBEAT];
 				e32_driver.send_packet(&send_packet_w_cmd, 5000).expect("Error sending heartbeat packet");
 				// our turn to read a packet
 				state = 1;
@@ -248,7 +323,7 @@ fn main() {
 			// send retry packet
 			6 => {
 				println!("send retry packet (6)");
-				let mut send_packet_w_cmd = vec![CMD_BYTE_RETRY];
+				let send_packet_w_cmd = vec![CMD_BYTE_RETRY];
 				e32_driver.send_packet(&send_packet_w_cmd, 5000).expect("Error sending heartbeat packet");
 				// our turn to read a packet
 				state = 1;
